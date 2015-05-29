eaf9814daacccd8128faadab66e19cde6219e2cb
Julie Kuehl <julie@juliekuehl.com>
2015-05-29 08:13:10 -0500

    imported code from Auto Generate Title plugin

--- fullname-post-title.php
+++ fullname-post-title.php
@@ -10,3 +10,25 @@ License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
+function agt_shortcode( $atts, $content = null ) {
+	return $content;
+}
+add_shortcode("agt", "agt_shortcode");
+
+add_filter( 'wp_insert_post_data' , 'auto_generate_title' , '99', 2 );
+function auto_generate_title( $data , $postarr )
+{
+	$title_of_post = ltrim($data['post_title']);
+	$length_of_title = strlen($title_of_post);
+	$postdata = ltrim($data['post_content']);
+	if($length_of_title<1)
+	{
+		$matchPattern = "/\[agt\](.*?)\[\/agt\]/";
+		preg_match_all($matchPattern, $postdata, $foundIt, PREG_PATTERN_ORDER);
+		if (isset($foundIt[1][0]))
+			$data['post_title'] =  $foundIt[1][0];
+		else
+			$data['post_title'] = substr($postdata,0,30);
+	}
+	return $data;
+}
