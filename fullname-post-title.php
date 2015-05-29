08b8d530cfec56a52e086bbf6ad37aaaef3c4a52
Julie Kuehl <julie@juliekuehl.com>
2015-05-29 08:12:28 -0500

    initial commit

---
+++ fullname-post-title.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Fullname Post Title (Pods)
+Description: A custom plugin to automatically generate a post title using the "fullname" field from Pods.
+Version: 1.0
+Author: juliekuehl
+Author URI: http://juliekuehl.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
